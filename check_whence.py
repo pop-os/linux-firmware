f6550448f114d6b4f6f482143d2668810eade5b4
Juerg Haefliger <juergh@canonical.com>
2022-05-10 16:28:11 +0200

    UBUNTU: SAUCE: Revert "UBUNTU: SAUCE: check_whence.py: Ignore ea/ directory"
    This reverts commit 892b161d643ee072d4fa904a977b5ecc053783be.
    
    The repo now contains pre-compiled echoaudio firmware so the ea/ directory
    must no longer be ignored.
    
    Signed-off-by: Juerg Haefliger <juergh@canonical.com>

--- check_whence.py
+++ check_whence.py
@@ -145,8 +145,6 @@ def main():
             ret = 1
 
     for name in sorted(list(known_files - git_files) if len(git_files) else list()):
-        if name.startswith("ea/"):
-            continue
         sys.stderr.write("E: %s listed in WHENCE does not exist\n" % name)
         ret = 1
 
